1e1ba5396bb905f5dfef013286084bb48e583a6d
Nikesh Gamal <nikesh.gamal24@gmail.com>
2026-04-28 12:14:31 +0545

    Sliding window intitial commit

--- 02. Sliding WIndow/src/MaxSumOfSubArray.java
+++
@@ -1,5 +0,0 @@
-class MaxSumOfSubArray {
-    public static void main(String [] args){
-        System.out.println("Hello world");
-    }
-}
---
+++ 02. Sliding WIndow/src/MinSubArrayLen.java
@@ -0,0 +1,9 @@
+class MinSubArrayLen {
+    public static void main(String [] args){
+        System.out.println();
+    }
+
+    public int minSubArrayLen(int target, int [] array){
+        return 0;
+    }
+}
