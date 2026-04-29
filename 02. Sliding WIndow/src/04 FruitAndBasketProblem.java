d134bd1d764216b2ce7e630ab9c5862b94d8672a
Nikesh Gamal <nikesh.gamal24@gmail.com>
2026-04-29 13:59:56 +0545

    fruit And BasketProblem Sliding Window: 2 Pointers

--- 02. Sliding WIndow/src/04 FruitAndBasketProblem.java
+++ 02. Sliding WIndow/src/04 FruitAndBasketProblem.java
@@ -31,12 +31,48 @@
 //If we had started at the first tree, we would only pick from trees [1,2].
 
 
- class FruitAndBasketProblem {
+import java.util.HashMap;
+
+class FruitAndBasketProblem {
     public static void main(String [] args){
+        int [] fruits1 = {1,2,1};
+        int [] fruits2 = {0,1,2,2};
+        int [] fruits3  = {1,2,3,2,2};
+        System.out.println("Output 1: "+ fruitAndBasketProblem(fruits1));
+        System.out.println("Output 2: "+ fruitAndBasketProblem(fruits2));
+        System.out.println("Output 3: "+ fruitAndBasketProblem(fruits3));
 
     }
 
-    public static int fruitAndBasketProblem(){
-        return  0;
+    public static int fruitAndBasketProblem(int [] fruits){
+        //declare necessary variable
+        int low = 0;
+        int high;
+        int res = Integer.MIN_VALUE;
+        int noOfBasket = 2;
+        // HashMap
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (high = 0; high < fruits.length; high++) {
+            // get the elements and keep count on hash map
+            // [1,2,1]
+            frequencyMap.put(fruits[high],frequencyMap.getOrDefault(fruits[high],0)+1);
+
+            // check if the count exceed two(number of basket) our maximum limit
+            while (frequencyMap.size() > noOfBasket){
+                //clear the fruits from the hashtable
+                frequencyMap.put(fruits[low],frequencyMap.getOrDefault(fruits[low],0)-1);
+
+                if(frequencyMap.getOrDefault(fruits[low],0) == 0){
+                    frequencyMap.remove(fruits[low]);
+                }
+
+                low++;
+            }
+
+            int len = high - low + 1;
+            res = Math.max(res, len);
+        }
+        return  res;
     }
 }
