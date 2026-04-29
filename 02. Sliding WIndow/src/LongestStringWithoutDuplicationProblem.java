053236226da6e783737d099857e70f7faeb02123
Nikesh Gamal <nikesh.gamal24@gmail.com>
2026-04-29 14:36:06 +0545

    Longest String Without Duplication Problem: Sliding window / 2 Pointers problem

--- 02. Sliding WIndow/src/LongestStringWithoutDuplicationProblem.java
+++ 02. Sliding WIndow/src/LongestStringWithoutDuplicationProblem.java
@@ -18,12 +18,55 @@
 
 //Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
- class LongestStringWithoutDuplicationProblem {
+import javax.swing.*;
+import java.util.HashMap;
+
+class LongestStringWithoutDuplicationProblem {
      public static void main(String [] args){
+         String str1 = "abcabcbb";
+         int k1 = 3;
+
+         String str2 = "bbbbb";
+         int k2 = 2;
+
+         String str3 = "pwwkew";
+         int k3 = 2;
+
+         System.out.println("Output 1: "+ longestStringWithoutDuplicationProblem(str1));
+         System.out.println("Output 2: "+ longestStringWithoutDuplicationProblem(str2));
+         System.out.println("Output 3: "+ longestStringWithoutDuplicationProblem(str3));
 
      }
 
-     public static int longestStringWithoutDuplicationProblem(){
-         return  0;
+     public static int longestStringWithoutDuplicationProblem(String str){
+         int low =0;
+         int high;
+         int res = Integer.MIN_VALUE;
+
+         HashMap<String,Integer> frequencyMap = new HashMap<>();
+
+         for (high = 0; high < str.length(); high++) {
+             // add new character to the substring and increase the count in the hashtable
+             char currentChar = str.charAt(high);
+             frequencyMap.put(String.valueOf(currentChar),frequencyMap.
+                     getOrDefault(String.valueOf(currentChar),0)+1);
+
+             // check if the window size is greater than that of frequencyMap size then there are duplicate elements
+             // so need start eliminating the elemens from initial pointers--low
+             int window = high -low+1;
+             while(window > frequencyMap.size()){
+                 char leftChar = str.charAt(low);
+                 frequencyMap.put(String.valueOf(leftChar),frequencyMap.
+                         getOrDefault(String.valueOf(leftChar),0)-1);
+                 if(frequencyMap.getOrDefault(String.valueOf(leftChar),0) == 0){
+                     frequencyMap.remove(String.valueOf(leftChar));
+                 }
+                 low++;
+                 window = high-low+1;
+             }
+             int len  = high - low + 1;
+             res = Math.max(res,len);
+         }
+         return  (res == Integer.MIN_VALUE)? -1 : res;
      }
 }
