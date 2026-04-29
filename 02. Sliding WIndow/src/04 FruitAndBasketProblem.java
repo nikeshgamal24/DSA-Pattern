f3594b797a139ec66241cac40a8592d6e040ea12
Nikesh Gamal <nikesh.gamal24@gmail.com>
2026-04-29 11:56:54 +0545

    setup code for fruit and basket coding problem

--- 02. Sliding WIndow/src/04 FruitAndBasketProblem.java
+++ 02. Sliding WIndow/src/04 FruitAndBasketProblem.java
@@ -1,2 +1,9 @@
-public class FruitAndBasketProblem {
+ class FruitAndBasketProblem {
+    public static void main(String [] args){
+
+    }
+
+    public static int fruitAndBasketProblem(){
+        return  0;
+    }
 }
