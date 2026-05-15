e1acf7d833e5d1e42a1a660823effb13d7c9dda8
Nikesh Gamal <nikesh.gamal24@gmail.com>
2026-05-15 10:30:26 +0545

    hasCycle better solution

--- 03. Fast-Slow Pointers/src/LinkedListCycle.java
+++ 03. Fast-Slow Pointers/src/LinkedListCycle.java
@@ -75,7 +75,7 @@ public class LinkedListCycle {
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast.next != null && fast.next.next != null){
+        while(fast != null && fast.next != null){
           slow = slow.next;
           fast = fast.next.next;
 
