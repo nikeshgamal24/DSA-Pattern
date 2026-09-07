36df14a4a28060e66fa4ad2dfbb2632ac631bfc4
Nikesh Gamal <nikesh.gamal24@gmail.com>
2026-09-07 11:40:42 -0400

    Prefix Sum Pattern: SubArray Sum Equals to K solution

--- 05. Prefix Sum/src/02 SubArraySumEqualsK.java
+++ 05. Prefix Sum/src/02 SubArraySumEqualsK.java
@@ -14,18 +14,33 @@
 //Output: 2
 
 
+
+import java.util.HashMap;
+
 class SubArraySumEqualsK {
     public static int subarraySum(int[] nums, int k) {
-        
-        return 0;
+        int sum =0;
+        int res = 0;
+        HashMap<Integer,Integer> hashmap = new HashMap<>();
+        hashmap.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            int quest = sum -k;
+            res += hashmap.getOrDefault(quest,0);
+            hashmap.put(sum,hashmap.getOrDefault(sum,0)+1);
+        }
+        return res;
     }
 
     public static void main(String[] args){
         int[] nums1={1,1,1};
-        int[] nums2={1,2,3};
+        int k1=2;
 
-        System.out.println("Output 1: " + subarraySum(nums1));
-        System.out.println("Output 2: " + subarraySum(nums2));
+        int[] nums2={1,2,3};
+        int k2=3;
+
+        System.out.println("Output 1: " + subarraySum(nums1,k1));
+        System.out.println("Output 2: " + subarraySum(nums2,k2));
 
     }
 }
