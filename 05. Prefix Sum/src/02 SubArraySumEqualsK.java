3162d5f191271b6427ff5644322f26e88f415339
Nikesh Gamal <nikesh.gamal24@gmail.com>
2026-09-07 11:31:43 -0400

    Prefix Sum Pattern: SubArray Sum Equals to K

---
+++ 05. Prefix Sum/src/02 SubArraySumEqualsK.java
@@ -0,0 +1,31 @@
+//560. Subarray Sum Equals K
+//Given an array of integers nums and an integer k,
+// return the total number of subarrays whose sum equals to k.
+//
+//A subarray is a contiguous non-empty sequence of elements within an array.
+
+//Example 1:
+//
+//Input: nums = [1,1,1], k = 2
+//Output: 2
+//Example 2:
+//
+//Input: nums = [1,2,3], k = 3
+//Output: 2
+
+
+class SubArraySumEqualsK {
+    public static int subarraySum(int[] nums, int k) {
+        
+        return 0;
+    }
+
+    public static void main(String[] args){
+        int[] nums1={1,1,1};
+        int[] nums2={1,2,3};
+
+        System.out.println("Output 1: " + subarraySum(nums1));
+        System.out.println("Output 2: " + subarraySum(nums2));
+
+    }
+}
